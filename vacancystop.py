adae61a1b289f19b307d7bbe9a28650b1cdc8373
Maxim Yankevich <max.yankevich@gmail.com>
2015-12-16 14:52:57 +0300

    1

---
+++ vacancystop.py
@@ -0,0 +1,22 @@
+﻿import re
+
+vacancies = []
+patternTypeID = 'vacancy_id":"\d*'
+patternID = 'vacancy_id":"'
+
+for line in open('error1.log'):
+	L = str(line)
+	forVac = re.search(patternTypeID,L)
+	if forVac != None:
+	    rowVacID = str(forVac.group())
+	vacID = rowVacID.replace(patternID, '')
+	if vacID != '':
+	    vacancies.append(vacID)
+	
+uniqVacancies = list(set(vacancies)) # убираем неуникальные номера вакансий
+
+output = open(r'todel.html', 'w')
+for line in uniqVacancies:
+    #print(line)
+	output.write('<a href="http://rdw.ru/partner/manager/vacancy/'+line+'" target="_blank">'+line+'</a> ')
+
